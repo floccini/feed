58ab65d3097fe1bd9047a42a0327d00d2d41f970
floccini <giovannifossati11@gmail.com>
2022-06-21 09:15:34 -0300

    finished layout

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/floccini.png"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Giovanni Fossati</strong>
+                            <time title="08 de Junho às 15:00" dateTime="2022-06-08 14:50:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
